1ff3de455661f13cdca2d358566dd0dff233a177
Hamilton Chapman <hamchapman@gmail.com>
2014-01-20 15:29:56 +0000

    Language test passing

--- helpers/language_helper.rb
+++ helpers/language_helper.rb
@@ -6,4 +6,19 @@ module LanguageHelper
     Net::HTTP.get_response(URI(uri))
   end
 
+  def parse_response response
+    JSON.parse(response.body)
+  end
+
+  def get_repos uri
+    response = get_response_from uri
+    return parse_response response if !response.message.include? "Not Found"
+    false
+  end
+
+  def get_array_of_languages repos
+    languages = repos.map {|repo| repo['language'] }
+    languages.delete_if { |lang| lang.nil? }
+  end
+
 end
